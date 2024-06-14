98b1ccf16ae1d37052923763a248e0de39e0b66f
iturralde.javi <iturralde.javi@gmail.com>
2024-06-14 15:21:16 +0000

    funciones extra escondidas

--- Controlers/ControlesComu.py
+++ Controlers/ControlesComu.py
@@ -1,7 +1,13 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, ConversationHandler, filters, CallbackContext, CallbackQueryHandler
 from Controlers.Funciones import *
 import math
+import logging
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 
 def decir_bobadas():
@@ -9,7 +15,7 @@ def decir_bobadas():
     return mensaje
 
 
-OBTENER_GRUPOS, PRUEBA_OTRA_VEZ, VER_BROS = range(3)
+OBTENER_GRUPOS, PRUEBA_OTRA_VEZ = range(2)
 
 keyboard = InlineKeyboardMarkup([
 # Suggested code may be subject to a license. Learn more: ~LicenseLog:2565311830.
@@ -45,33 +51,7 @@ class ListaComunidad:
         return mensaje
 
 
-    @staticmethod
-    async def obtener_grupos(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        context.user_data["groups"] = update.message.text
-
-        try:
-            numero = int(context.user_data['groups'])
-        
-            mensaje = ""
-            message = ""
-            
-            if numero >= 0:
-                mensaje = f"Vale, voy a hacer {numero} grupos de comunidad"
-                await update.message.reply_text(mensaje)
-
-                grupos_creados = crear_grupos(numero, diccionario)
-                
-                for i, grupo in enumerate(grupos_creados):
-                    message += f"Grupo {i+1}: "
-                    message += ", ".join(grupo) + "\n"
-                
-                return message
-        except ValueError:
-            await update.message.reply_text("Por favor, ingresa un número válido.")
-            return 
-        
-        return 
-
+    
     @staticmethod
     async def boton_callback(update: Update, context: CallbackContext):
         await update.callback_query.answer()
@@ -135,4 +115,164 @@ class ListaComunidad:
         await update.message.reply_sticker("Miracolo.webp", reply_markup=keyboard3)
         return
 
+async def conv(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Hola, ¿En que puedo ayudarte?")
+    return OBTENER_GRUPOS
+
+
+async def obtener_grupos(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    context.user_data["groups"] = update.message.text
+
+    try:
+        numero = int(context.user_data['groups'])
+            
+        mensaje = ""
+        message = ""
+                
+        if numero >= 0:
+            mensaje = f"Vale, voy a hacer {numero} grupos de comunidad"
+            await update.message.reply_text(mensaje)
+
+            grupos_creados = crear_grupos(numero, diccionario)
+                    
+            for i, grupo in enumerate(grupos_creados):
+                message += f"Grupo {i+1}: "
+                message += ", ".join(grupo) + "\n"
+                    
+            return await update.message.reply_text(message)
+        
+    except ValueError:
+        await update.message.reply_text("Por favor, ingresa un número válido.")
+        return 
+        
+    
+
+        
+
+
+async def cancel_conversation(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Conversación cancelada.")
+    return ConversationHandler.END
+
+        
+
  
+
+# pruebaConversacion = ConversationHandler(
+#     entry_points=[CommandHandler("conv", conv)],
+#     states={
+#         OBTENER_GRUPOS: [MessageHandler(filters.TEXT & ~filters.COMMAND, obtener_grupos)]
+#     },
+#     fallbacks=[ CommandHandler("cancel", cancel_conversation)]
+# )
+
+async def inicio(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    reply_keyboard = [["Boy", "Girl", "Other"]]
+
+    await update.message.reply_text(
+        "Hi! My name is Professor Bot. I will hold a conversation with you. "
+        "Send /cancel to stop talking to me.\n\n"
+        "Are you a boy or a girl?",
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard, one_time_keyboard=True, input_field_placeholder="Boy or Girl?"
+        ),
+    )
+    return GENDER
+
+async def gender(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
+    context.user_data["gender"] = update.message.text
+    logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    await update.message.reply_text(
+        "I see! Please send me a photo of yourself, "
+        "so I know what you look like, or send /skip if you don't want to.",
+        reply_markup=ReplyKeyboardRemove(),
+    )
+
+    return PHOTO
+
+async def photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the photo and asks for a location."""
+    user = update.message.from_user
+    photo_file = await update.message.photo[-1].get_file()
+    await photo_file.download_to_drive("user_photo.jpg")
+    logger.info("Photo of %s: %s", user.first_name, "user_photo.jpg")
+    await update.message.reply_text(
+        "Gorgeous! Now, send me your location please, or send /skip if you don't want to."
+    )
+
+    return LOCATION
+
+
+async def skip_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Skips the photo and asks for a location."""
+    user = update.message.from_user
+    logger.info("User %s did not send a photo.", user.first_name)
+    await update.message.reply_text(
+        "I bet you look great! Now, send me your location please, or send /skip."
+    )
+
+    return LOCATION
+
+
+async def location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the location and asks for some info about the user."""
+    user = update.message.from_user
+    user_location = update.message.location
+    logger.info(
+        "Location of %s: %f / %f", user.first_name, user_location.latitude, user_location.longitude
+    )
+    await update.message.reply_text(
+        "Maybe I can visit you sometime! At last, tell me something about yourself."
+    )
+
+    return BIO
+
+
+async def skip_location(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Skips the location and asks for info about the user."""
+    user = update.message.from_user
+    logger.info("User %s did not send a location.", user.first_name)
+    await update.message.reply_text(
+        "You seem a bit paranoid! At last, tell me something about yourself."
+    )
+
+    return BIO
+
+
+async def bio(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Stores the info about the user and ends the conversation."""
+    user = update.message.from_user
+    logger.info("Bio of %s: %s", user.first_name, update.message.text)
+    await update.message.reply_text("Thank you! I hope we can talk again some day.")
+
+    return ConversationHandler.END
+
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
+
+    return ConversationHandler.END
+
+GENDER, PHOTO, LOCATION, BIO = range(4)
+
+conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("inicio", inicio)],
+        states={
+            GENDER: [MessageHandler(filters.Regex("^(Boy|Girl|Other)$"), gender)],
+            PHOTO: [MessageHandler(filters.PHOTO, photo), CommandHandler("skip", skip_photo)],
+            LOCATION: [
+                MessageHandler(filters.LOCATION, location),
+                CommandHandler("skip", skip_location),
+            ],
+            BIO: [MessageHandler(filters.TEXT & ~filters.COMMAND, bio)],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+
+    
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, ConversationHandler, filters, CallbackContext, CallbackQueryHandler
 import random
-from Controlers.ControlesComu import OBTENER_GRUPOS, PRUEBA_OTRA_VEZ, ListaComunidad
+from Controlers.ControlesComu import *
 
 
 TOKEN = "7442684904:AAF0JtIzYB4zoff6B-STm9ndvUGmkvq3OPM"
@@ -13,7 +13,11 @@ application.add_handler( CommandHandler( "start", ListaComunidad.say_hello) )
 application.add_handler( CommandHandler( "respo", ListaComunidad.respo) )
 application.add_handler( CommandHandler( "miracolo", ListaComunidad.miracolo) )
 application.add_handler( CommandHandler( "ver_hermanos", ListaComunidad.see_bros) )
+application.add_handler( CommandHandler( "conv", conv))
 application.add_handler( CallbackQueryHandler(ListaComunidad.boton_callback) )
+
+# application.add_handler( pruebaConversacion )
+application.add_handler(conv_handler)
 
 
 application.run_polling(allowed_updates=Update.ALL_TYPES)
